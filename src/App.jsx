5fe20e30072992e236f594706f9047d1a963b71e
jovanlekic12 <lekicjovan1212@gmail.com>
2024-12-30 23:41:51 +0100

    add navbar and style

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { Route, Routes } from "react-router";
+import Home from "./components/Home";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-    </>
-  )
+    <Routes>
+      <Route index element={<Home />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+import Navbar from "./navbar";
+
+function Home() {
+  return (
+    <main className="main__container">
+      <Navbar></Navbar>
+    </main>
+  );
+}
+
+export default Home;
