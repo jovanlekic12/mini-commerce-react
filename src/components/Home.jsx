7362b54b98ebe6c5b9d12d21e59767ed85cf977a
jovanlekic12 <lekicjovan1212@gmail.com>
2024-12-31 17:12:10 +0100

    add search bar

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,10 @@
 import Navbar from "./navbar";
-
+import SearchBar from "./SearchBar";
 function Home() {
   return (
     <main className="main__container">
       <Navbar></Navbar>
+      <SearchBar></SearchBar>
     </main>
   );
 }
---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,10 @@
+function SearchBar() {
+  return (
+    <div className="search__bar">
+      <h1>Search product</h1>
+      <input type="text" />
+    </div>
+  );
+}
+
+export default SearchBar;
